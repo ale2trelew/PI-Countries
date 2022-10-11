c6b275be4a06b8747ef57a0b72dc995e8063790a
ale2trelew <ale2trelew@gmail.com>
2022-10-11 18:43:50 -0300

    ruta de paises hecha

---
+++ api/src/routes/countries.js
@@ -0,0 +1,65 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+
+const sequelize = require("sequelize");
+const { Op } = require("sequelize");
+
+const { Country, Season, Activity } = requeire("../db.js");
+
+router.use(express.json());
+
+router.get("/", async (req, res) => {
+    try {
+        let { name } = req.query;
+        if (name === "" || name === undefined) {
+            let data = await Country.findAll({
+                include: [{ model: Activity }],
+            });
+            res.json(data);
+        } else if (name) {
+            const country = await Country.findAll({
+                where: sequelize.where(
+                    sequelize.fn("unaccent", sequelize.col("country.name")),
+                    {
+                        [Op.iLike]:
+                            name
+                                .normalize("NFD")
+                                .replace(/[\u0300-\u036f]/g, "")
+                                .toLowerCase() + "%",
+                    }
+                ),
+                include: [{ model: Activity }],
+            });
+            if (country.length !== 0) {
+                res.json(country);
+            } else {
+                res.json([]);
+            }
+        }
+    } catch (e) {
+        console.log("/routes/countries.js get / error: ", e);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    let { id } = req.params;
+    try {
+        const pais = await Country.findOne({
+            where: {id:id},
+            include: [{ 
+                model: Activity,
+                include: [{ model: Season }]
+            }],
+        })
+        if (pais !== null) {
+            res.json(pais);
+        } else {
+            res.json("Pais no encontrado");
+        }
+    } catch (e) {
+        console.log("/routes/countries.js get /:id error: ", e);
+    }
+});
+
+module.exports = router;
