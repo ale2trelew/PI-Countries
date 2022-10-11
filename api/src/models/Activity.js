5a548a79a070a6eee0c3478563f9ba488851e57e
ale2trelew <ale2trelew@gmail.com>
2022-10-11 17:40:56 -0300

    Modelos hechos

---
+++ api/src/models/Activity.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('activity', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    difficulty: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        values: ["Principiante", "Aficionado", "Normal", "Profesional", "Experto"],
+    },
+    duration: {
+        type: DataTypes.REAL(2, 2),
+        allowNull: false,
+    },
+  });
+};
